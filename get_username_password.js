abd32feb2afa606e4312ab27597533025a740bba
lazbord <153539549+lazbord@users.noreply.github.com>
2024-01-07 19:28:37 +0100

    Delete get_username_password.js

--- get_username_password.js
+++
@@ -1,8 +0,0 @@
-function get_username() {
-    document.getElementById("username_result").innerText = "ok";
-    //suppose to work on : https://vinciracingteam.fr/se-connecter/
-    //username = document.getElementById('username-200').value;
-    //document.getElementById("username_result").innerText = "The username entered : " + username;
-}
-
-document.getElementById("username").addEventListener("click", get_username);
