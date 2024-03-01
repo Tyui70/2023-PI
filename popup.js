7d6e6b8009432a6874c01244d31bd1ad95259af0
tiphaine kachkachi <tiphaine.kachkachi@edu.devinci.fr>
2024-03-01 16:14:11 +0100

    Séparation url automatisation/partie manuelle

--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ function showUsername() {
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentUrl = tabs[0].url;
-    document.getElementById("url_input").value = currentUrl;
+    document.getElementById("Link_url").textContent = currentUrl;
 });
 
 document.getElementById("UsernameBtn").addEventListener("click", showUsername);
